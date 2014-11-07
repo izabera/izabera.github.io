1b734c83d8a7a10c5471b519e27ba92a50d42282
izabera <izaberina@gmail.com>
2014-11-07 15:16:26 +0100

    Fri Nov  7 15:16:26 CET 2014

--- makeblog.sh
+++ makeblog.sh
@@ -92,7 +92,7 @@ echo
 cd ../temp
 count=0
 printbar $count $total "  Markdown conversion"
-for file in $(sort list); do
+for file in $(sort -r list); do
   title="$(head -n1 "$file")"
   newfile="$(echo "$title" | tr 'A-Z ' 'a-z-' | tr -dc 'a-z-')" #with no extension
   timestamp="$(date '+%c' -d @${file:2:10})"
